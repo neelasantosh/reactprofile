c62630ce16d5d6146389b466e0fa37590b22f10e
neelasantosh <neela.santosh2405@gmail.com>
2020-03-25 15:08:14 +0530

    Updates

---
+++ precache-manifest.9389db177455675eb7ae2b6d9c1336b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c40fdf9d6f6cca2bd931b8b5f7889aa",
+    "url": "/santoshprofile/index.html"
+  },
+  {
+    "revision": "aeaf0e7672f7a7acde95",
+    "url": "/santoshprofile/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "644619407d00e9dc6619",
+    "url": "/santoshprofile/static/css/main.f0bc0805.chunk.css"
+  },
+  {
+    "revision": "aeaf0e7672f7a7acde95",
+    "url": "/santoshprofile/static/js/2.eb26123d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/santoshprofile/static/js/2.eb26123d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644619407d00e9dc6619",
+    "url": "/santoshprofile/static/js/main.6d2a3d95.chunk.js"
+  },
+  {
+    "revision": "c95a16f242a976189947",
+    "url": "/santoshprofile/static/js/runtime-main.7d2d419f.js"
+  },
+  {
+    "revision": "a3ef69833e650744d912394f06a3d538",
+    "url": "/santoshprofile/static/media/santu.a3ef6983.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/themoviedb/precache-manifest.f615cc9b00a7081d43b3bb0b5f2e5c2a.js"
+  "/santoshprofile/precache-manifest.9389db177455675eb7ae2b6d9c1336b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/themoviedb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/santoshprofile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
