7b0c7587ed084919dcb225c1633e44d3a60d5f83
neelasantosh <neela.santosh2405@gmail.com>
2020-03-25 17:24:31 +0530

    implemented hasrouter

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
